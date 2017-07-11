31f593664a262ad7a4b8ba378b2a8604ff8df2cd
agonzalez123456 <agonzalez@conservicios.cl>
2017-07-11 13:09:38 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+  	$dbhost="172.30.1.210";	
+	$dbusername='sa';	
+	$dbuserpass='lip0';	
+	$dbname='conavicoop';
+	$db= mssql_connect ($dbhost, $dbusername, $dbuserpass);
+    mssql_select_db($dbname);	
+  	//	$qry="SET ANSI_NULLS, QUOTED_IDENTIFIER, CONCAT_NULL_YIELDS_NULL, ANSI_WARNINGS, ANSI_PADDING ON";
+	//	$result = mssql_query($qry, $db);// or die(mssql_error());			
+		
+		
+		
+ 		$qry = "getAll_datosAgente2 '01', '201507'";
+ 		$result = mssql_query($qry, $db);// or die(mssql_error());			
+		while($row = mssql_fetch_array($result)){
+			echo $row[1].'<br>';
+			
+		}
+
+		
+		
+?>
